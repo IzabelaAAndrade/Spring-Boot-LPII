3a29ee1084560988a9f7bcbe6741517e122f79af
Izabela A. Andrade <contatobelaaa@gmail.com>
2022-01-07 19:02:44 -0300

    Questionário 16/12/21

--- eventosapp/src/main/java/com/eventosapp/controllers/EventoController.java
+++ eventosapp/src/main/java/com/eventosapp/controllers/EventoController.java
@@ -1,14 +1,38 @@
 package com.eventosapp.controllers;
 
+import com.eventosapp.models.Evento;
+import com.eventosapp.repository.EventoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
 
-    @RequestMapping("/cadastrarEvento")
+    @Autowired
+    private EventoRepository er;
+
+    @RequestMapping(value="/cadastrarEvento", method= RequestMethod.GET)
     public String form(){
         return "eventos/formEvento";
+    }
+
+    @RequestMapping(value="/cadastrarEvento", method= RequestMethod.POST)
+    public String form(Evento e){
+        er.save(e);
+
+        return "redirect:cadastrarEvento";
+    }
+
+    @RequestMapping("/eventos")
+    public ModelAndView listarEventos(){
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Evento> eventosCadastrados = er.findAll();
+        mv.addObject("evento", eventosCadastrados);
+
+        return mv;
     }
 
 }
