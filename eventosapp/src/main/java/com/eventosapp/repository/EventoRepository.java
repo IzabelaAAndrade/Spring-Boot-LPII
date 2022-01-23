8355d2be87c78186251500d5994939dfa0d79c11
Izabela A. Andrade <contatobelaaa@gmail.com>
2022-01-22 21:26:20 -0300

    Questionário 06/01/22

--- eventosapp/src/main/java/com/eventosapp/repository/EventoRepository.java
+++ eventosapp/src/main/java/com/eventosapp/repository/EventoRepository.java
@@ -4,4 +4,5 @@ import com.eventosapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EventoRepository extends CrudRepository<Evento, String> {
+    Evento findByCodigo(long codigo); // Para a geração de links para páginas específicas de cada evento conforme seu código
 }
